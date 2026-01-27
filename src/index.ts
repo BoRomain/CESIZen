c36ae4982b0004b7fe5a7f93fab5c32925129b71
BoRomain <romain.bouffandeau@gmail.com>
2026-01-27 19:31:23 +0100

    ajout d'autres routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import utilisateur from "./routes/Utilisateur.js";
+import information from "./routes/Information.js";
+import activiteDetente from "./routes/ActiviteDetente.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger-output.json" with { type: "json" };
 
@@ -9,6 +11,8 @@ const port: number = 3000;
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/utilisateur", utilisateur);
+app.use("/information", information);
+app.use("/activiteDetente", activiteDetente);
 
 app.get("/", (req, res) => {
   res.send("CESIZEN actif !");
