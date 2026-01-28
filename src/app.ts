fd225cf9a7df8896a2a98dbd746e9f1a4f254f24
BoRomain <romain.bouffandeau@gmail.com>
2026-01-28 13:30:00 +0100

    setup prisma

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,9 @@ import express from "express";
 import utilisateur from "./routes/Utilisateur.js";
 import information from "./routes/Information.js";
 import activiteDetente from "./routes/ActiviteDetente.js";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger-output.json" with { type: "json" };
 
 const app = express();
 const port: number = 3000;
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/utilisateur", utilisateur);
 app.use("/information", information);
--- src/routes/Utilisateur.ts
+++ src/routes/Utilisateur.ts
@@ -1,9 +1,61 @@
 import { Router } from "express";
+import prisma from "../database.js";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Utilisateur");
+router.get("/", async (req, res) => {
+  const users = await prisma.utilisateur.findMany();
+  res.json(users);
+});
+
+router.post("/create", async (req, res) => {
+  const { nom, prenom, email, motDePasse} = req.body;
+
+  if (!nom || !prenom || !email || !motDePasse) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  const hashedPassword = await bcrypt.hash(motDePasse, 10);
+
+  const user = await prisma.utilisateur.create({
+    data: {
+      nom,
+      prenom,
+      email,
+      motDePasse: hashedPassword,
+      role: "user",
+      dateCreation: new Date(),
+      status: true
+    }
+  })
+  res.json("User created successfully");
+});
+
+router.post("/disable/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.utilisateur.update({
+    where: {
+      id: Number(id)
+    },
+    data: {
+      status: false
+    }
+  });
+  res.json(user);
+});
+
+router.post("/enable/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.utilisateur.update({
+    where: {
+      id: Number(id)
+    },
+    data: {
+      status: true
+    }
+  });
+  res.json(user);
 });
 
 export default router;
